ef0914b9ac50265d70a02db91641a3953f86a796
blelump <blelump@martex.nom.pl>
2026-03-06 17:02:45 +0100

    feat: Run on AstroWind

---
+++ src/navigation.ts
@@ -0,0 +1,35 @@
+export const headerData = {
+  links: [
+    {
+      text: 'Features',
+      href: '/#features',
+    },
+    {
+      text: 'How It Works',
+      href: '/#how-it-works',
+    },
+    {
+      text: 'Contact',
+      href: '/#contact',
+    },
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '/#features' },
+        { text: 'How It Works', href: '/#how-it-works' },
+        { text: 'Contact', href: '/#contact' },
+      ],
+    },
+  ],
+  secondaryLinks: [],
+  socialLinks: [],
+  footNote: `
+    &copy; 2026 Sigmion. All rights reserved.
+  `,
+};
